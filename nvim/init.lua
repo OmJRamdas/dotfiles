8bbf83542fa985c7cc10e40ebe4c2260305d694e
Om Ramdas <om.ramdas@tetramem.com>
2026-06-30 18:05:22 -0700

    fix nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,26 +18,29 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-  { "neovim/nvim-lspconfig" },
-  { "williamboman/mason.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }, cond = vim.fn.has("nvim-0.11") == 1 },
+  { "neovim/nvim-lspconfig", cond = vim.fn.has("nvim-0.10") == 1 },
+  { "williamboman/mason.nvim", cond = vim.fn.has("nvim-0.10") == 1 },
+  { "williamboman/mason-lspconfig.nvim", cond = vim.fn.has("nvim-0.10") == 1 },
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 })
 
--- mason
-require("mason").setup()
-require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "pyright" } })
-
--- lsp
-vim.lsp.config("lua_ls", {})
-vim.lsp.enable("lua_ls")
+-- mason + lsp (requires nvim 0.10+)
+if vim.fn.has("nvim-0.10") == 1 then
+  require("mason").setup()
+  require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "pyright" } })
+  local lspconfig = require("lspconfig")
+  lspconfig.lua_ls.setup({})
+  lspconfig.pyright.setup({})
+end
 
 -- telescope keymaps
-local t = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", t.find_files)
-vim.keymap.set("n", "<leader>fg", t.live_grep)
-vim.keymap.set("n", "<leader>fb", t.buffers)
+if vim.fn.has("nvim-0.11") == 1 then
+  local t = require("telescope.builtin")
+  vim.keymap.set("n", "<leader>ff", t.find_files)
+  vim.keymap.set("n", "<leader>fg", t.live_grep)
+  vim.keymap.set("n", "<leader>fb", t.buffers)
+end
 
 -- modules
 -- require("config.keymaps")
